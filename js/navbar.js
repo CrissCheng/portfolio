a775bf2928b83a4dae1aefce26373e40f691ce68
Criss Cheng <chrischengck10339@gmail.com>
2019-06-15 14:38:15 +0800

    rebase

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+$(function() {						
+    $(document).on('click', 'a.nav-link', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
